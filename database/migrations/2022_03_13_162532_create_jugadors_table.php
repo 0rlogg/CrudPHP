162c1fecac9002395fedec5fea3dcc3111a700c9
0rlogg <danimb1506@gmail.com>
2022-03-13 18:51:11 +0100

    Initial commit

---
+++ database/migrations/2022_03_13_162532_create_jugadors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJugadorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jugadors', function (Blueprint $table) {
+            $table->id();
+            $table->string('foto');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jugadors');
+    }
+}
